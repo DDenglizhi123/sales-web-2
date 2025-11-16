8209958acc0abe45a398429996f4553d30cc2fb9
DDenglizhi123 <neverforever@vip.qq.com>
2025-11-16 17:08:18 +0300

    update

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import ProductListView, ProductCreateView, ProductUpdateView, ProductDeleteView
+from .views import product_list
 urlpatterns = [
-    path('',ProductListView.as_view(), name='item_list'),
-    path('create/', ProductCreateView.as_view(), name='item_create'),
-    path('update/<int:pk>/', ProductUpdateView.as_view(), name='item_update'),
-    path('delete/<int:pk>/', ProductDeleteView.as_view(), name='item_delete'),
+    path('',product_list, name='item_list'),
+    # path('create/', ProductCreateView.as_view(), name='item_create'),
+    # path('update/<int:pk>/', ProductUpdateView.as_view(), name='item_update'),
+    # path('delete/<int:pk>/', ProductDeleteView.as_view(), name='item_delete'),
 ]
--- products/views.py
+++ products/views.py
@@ -4,38 +4,48 @@ from .models import Products
 from django.urls import reverse_lazy
 from .forms import ProductForm
 from django.db.models import Q
-# Create your views here.
-class ProductListView(ListView):
-    model = Products
-    template_name = 'products/product_list.html'
-    context_object_name = 'products'
-    fields = ['item_name', 'price', 'unit', 'stock_quantity', 'item_number', 'keyword1', 'keyword2', 'keyword3', 'weight_in_kg', 'packing_L', 'packing_W', 'packing_H']
-    paginate_by = 20  # Number of customers per page
+# class ProductListView(ListView):
+#     model = Products
+#     template_name = 'products/product_list.html'
+#     context_object_name = 'products'
+#     fields = ['item_name','base_price','unit','stock_quantity','created_at']
+#     paginate_by = 20  # Number of customers per page
     
-    #查询功能
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        search = self.request.GET.get('search')
-        if search:
-            queryset = queryset.filter(
-                Q(item_name__icontains=search) |
-                Q(keyword1__icontains=search) |
-                Q(keyword2__icontains=search) |
-                Q(keyword3__icontains=search)
-            )
-        return queryset
+#     #查询功能
+#     def get_queryset(self):
+#         queryset = super().get_queryset()
+#         search = self.request.GET.get('search')
+#         if search:
+#             queryset = queryset.filter(
+#                 Q(item_name__icontains=search) |
+#                 Q(keyword1__icontains=search) |
+#                 Q(keyword2__icontains=search) |
+#                 Q(keyword3__icontains=search)
+#             )
+#         return queryset
+def product_list(request):
+    query = request.GET.get('search', '')
+    if query:
+        products = Products.objects.filter(
+            Q(item_name__icontains=query) |
+            Q(keyword1__icontains=query) |
+            Q(keyword2__icontains=query) |
+            Q(keyword3__icontains=query)
+        )
+    else:
+        products = Products.objects.all()
+    return render(request, 'products/product_list.html', {'products': products, 'query': query})
+
+# class ProductCreateView(CreateView):
+#     model = Products
+#     template_name = 'products/product_form.html'
+#     form_class = ProductForm
+#     success_url = reverse_lazy('item_list')
 
 
-class ProductCreateView(CreateView):
-    model = Products
-    template_name = 'products/product_form.html'
-    form_class = ProductForm
-    success_url = reverse_lazy('item_list')
 
 
-
-
-class ProductUpdateView(UpdateView):
+# class ProductUpdateView(UpdateView):
     # model = Products
     # template_name = 'products/product_form.html'
     # context_object_name = 'product'
@@ -43,8 +53,8 @@ class ProductUpdateView(UpdateView):
     # success_url = '/item_list/'  # Redirect to the customer list after successful update
     pass
 
-class ProductDeleteView(DeleteView):
-    model = Products
-    template_name = 'products/product_delete.html'
-    context_object_name = 'products'
-    success_url = reverse_lazy('item_list')  # Redirect to the customer list after successful deletion
+# class ProductDeleteView(DeleteView):
+#     model = Products
+#     template_name = 'products/product_delete.html'
+#     context_object_name = 'products'
+#     success_url = reverse_lazy('item_list')  # Redirect to the customer list after successful deletion
